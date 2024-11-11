f0d8f8e6200d1105389f7750c3e2687cf3d4aa21
Salvatore Giampa <sv.giampa@gmail.com>
2024-11-11 19:04:14 +0100

    Removed TensorFlow test from entrypoint; added nvidia-smi test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+nvidia-smi
+
 if [ -e /projects/autorun.sh ]; then
     /projects/autorun.sh &
 fi
-
-python3 -c "import tensorflow as tf; tf.config.list_physical_devices()"
 
 /usr/sbin/sshd -D
 
