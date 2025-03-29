780f065510eea5f97a211604554d78d657a6bca7
Salvatore Giampa <sv.giampa@gmail.com>
2025-03-29 13:59:52 +0100

    Added some logging to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ if [ ! -d $WORKSPACE ]; then
 fi
 
 # setup devconf directory
+echo "Setting up .devconf directory"
 export DEVCONF=$(/devconf.sh | tail -1)
 
 # change current directory to workspace
@@ -17,21 +18,26 @@ echo "cd $WORKSPACE" >> ~/.bashrc
 echo "cd $WORKSPACE" >> ~/.profile
 
 # set correct permissions on .ssh folder
+echo "Setting correct permissions on ~/.ssh folder"
 chmod -R 700 ~/.ssh
 
 # generate SSH key if not present
 if [ ! -f ~/.ssh/id_rsa ]; then
+    echo "Creating id_rsa private key for SSH connection"
     ssh-keygen -b 4096 -f ~/.ssh/id_rsa -N '' < y
     cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 fi
 
 # run ssh daemon for building ssh clusters
+echo "Starting SSH daemon"
 /usr/sbin/sshd -D &
 
 # upgrade and run code-server
+echo "Starting code-server"
 /run_code_server.sh ${@} &
 
 # run autorun script
+echo "Starting $DEVCONF/autorun.sh"
 $DEVCONF/autorun.sh &
 
 # keep entrypoint script running
